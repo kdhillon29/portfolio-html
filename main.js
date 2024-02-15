8b95d9933fdaa01fcd094a1bb59165633b47dbdb
kanwar Dhillon <kdhillon469@gmail.com>
2024-02-15 14:35:18 +1100

    navbar html ,js and styles completed

---
+++ main.js
@@ -0,0 +1,22 @@
+//NAVBAR
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+const body = document.querySelector('body')
+const navLinks = navMenu.querySelectorAll('li');
+const nav = document.querySelector('nav')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+    body.classList.toggle("no-scroll")
+})
+
+function closeMenu() {
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+    body.classList.remove("no-scroll")
+}
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', closeMenu)
+})
